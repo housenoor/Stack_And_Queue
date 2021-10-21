b6583a2c71e11fde67f1cdc5f20bbba202e14b6d
Srikanth <srikanthh994@gmail.com>
2021-10-21 12:59:54 +0530

    Added:Method to Perform Peak and Pop Operations

--- MyStack.java
+++ MyStack.java
@@ -3,14 +3,30 @@ package com.stackandqueue;
 public class MyStack {
 	private MyNode top;
 
-	
-	  //This method is used to add elements to stack
-	 
+	// This method is used to add elements to stack
 	public void push(int element) {
 		MyNode node = new MyNode();
 		node.key = element;
 		node.next = top;
 		top = node;
+	}
+
+	/* This Method is used to return top element from the stack */
+	public int peak() {
+		System.out.println("Top element is " + top.key);
+		return top.key;
+	}
+
+	/* This method is used to delete element from the stack */
+	public void pop() {
+		if (top == null)
+			System.out.println("Stack empty");
+		else {
+			MyNode temp = top;
+			System.out.println("Popped out element is " + temp.key);
+			top = top.next;
+		}
+
 	}
 
 	/* This method is used to display the stack list */
