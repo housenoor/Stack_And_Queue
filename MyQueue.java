55f79c3ddddd3491225f1e0e3e4cad86d20b429c
Srikanth <srikanthh994@gmail.com>
2021-10-21 13:09:02 +0530

    Added:Method To Add Elements to Queue

---
+++ MyQueue.java
@@ -0,0 +1,34 @@
+package com.stackandqueue;
+
+public class MyQueue {
+	MyNode front;
+	MyNode rear;
+
+	// This Method is used to add elements to Queue
+
+	public void enqueue(int element) {
+		MyNode node = new MyNode();
+		node.key = element;
+		if (front == null) {
+			front = node;
+			rear = node;
+		} else {
+			rear.next = node;
+			rear = node;
+		}
+	}
+
+	/* This Method is used to display the elements in Queue. */
+	public void display() {
+		if (front == null)
+			System.out.println("Queue is empty");
+		else {
+			MyNode temp = front;
+			System.out.println("Queue list is -");
+			while (temp != null) {
+				System.out.println(temp.key);
+				temp = temp.next;
+			}
+		}
+	}
+}
